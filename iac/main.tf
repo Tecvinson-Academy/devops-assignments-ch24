1050db96ad31258d7881a85eb534298fa8a119d8
chiarasei <chiarasei.27@gmail.com>
2024-12-18 19:50:46 +0100

    chiarasei -  chiarasei super-linter.yml (#117)
    * Adding Nginx bash script
    
    * Adding Nginx bash script
    
    * Add files via upload
    
    super-linter.yml
    
    * Add files via upload
    
    * Add files via upload
    
    * update
    
    ---------
    
    Co-authored-by: chiara <chiara@gmail.com>
    Co-authored-by: Lawal Adekunle <lawaladekunle@gmail.com>

---
+++ iac/main.tf
@@ -0,0 +1,202 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-0dba2cb6798deb6d8"
+
+}
+variable "db_password" {
+  default = "password123"
+}
+
+
+resource "aws_security_group" "chiara_security_group" {
+  name        = "chiara-sg"
+  description = "My security group"
+
+  tags = {
+    Name = "chiara-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.chiara_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.chiara_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.chiara_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_instance" "tecvinson_nginx" {
+  count                  = 3
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.chiara_security_group.id]
+
+  user_data = file("${path.module}/nginx.sh")
+  tags = {
+    Name = "TecVinson Nginx"
+  }
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y nginx
+              sudo systemctl start nginx
+              EOF
+
+  tags = {
+    Name = "Nginx Instance"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_subnet" "private_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id] # Multi-AZ private subnets
+
+  tags = {
+    Name = "my DB subnet group"
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_name                = "mydb"
+  username               = "admin"
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  password               = var.db_password
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  tags = {
+    Name = "RDS MySQL"
+  }
+}
+
+output "nginx_public_ip" {
+  value = aws_instance.nginx.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.endpoint
+}
+
+
+ 
